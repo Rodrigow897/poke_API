7a8ef08f07e23c78592b9ffafa1b8247f9a40039
Rodrigo <rwesley2000@gmail.com>
2025-11-07 09:14:45 -0300

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import styles from './styles'
+
+export default function Index() {
+    return(
+        <SafeAreaView style={styles.container}>
+            <View></View>
+        </SafeAreaView>
+    )
+}
