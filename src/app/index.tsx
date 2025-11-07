3eac0d14c03f9695b14730a507aa2ea7e4e0eea9
Rodrigo <rwesley2000@gmail.com>
2025-11-07 10:10:11 -0300

    style: logo and icon

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,16 @@
-import { Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import styles from './styles'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { Image } from 'expo-image';
+import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import styles from './styles';
 
 export default function Index() {
     return(
         <SafeAreaView style={styles.container}>
-            <View></View>
+            <View style={styles.header}>
+                <MaterialIcons style={styles.icon} name="catching-pokemon" size={30} color="black" />
+                <Image style={styles.logo} source={require('../assets/images/ball.png')}/>
+            </View>
         </SafeAreaView>
     )
 }
--- src/app/styles.ts
+++ src/app/styles.ts
@@ -4,6 +4,27 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
         alignItems: 'center'
+    },
+    header:{
+        width: '100%',
+        height: 200,
+        borderWidth: 1,
+        borderColor: 'red',
+        justifyContent: 'center',
+    },
+    logo:{
+        height: 250,
+        width: 250,
+        alignSelf: 'flex-end',
+        position: 'absolute',
+        right: -80,
+        top: -25,
+        opacity: 0.08
+    },
+    icon:{
+        alignSelf: 'flex-end',
+        paddingRight: 30,
+        paddingTop: 5
     }
 });
 
